dd6674eb2a660e160641adbf696808c0dee7c6f2
mortuosplango <holger@music-nerds.net>
2010-10-19 17:26:31 +0200

    more rows, ability to change the sample

--- clss.lisp
+++ clss.lisp
@@ -5,10 +5,10 @@
 (in-package :sdl-gfx-examples)
 
 (defparameter *padding* 30)
-(defparameter *width* 600)
-(defparameter *height* 300)
 (defparameter *sample* nil)
 (defparameter *top-padding* 20)
+(defparameter *width* 800)
+(defparameter *height* 300)
 (defparameter *mixer-opened* nil)
 (defparameter *sample* 0)
 (defparameter *samples* '())
@@ -43,60 +43,54 @@
    (sample-instance :accessor sample-instance :initarg :sample-instance)
    ))
 
+(defgeneric load-sample (seq-row))
 (defmethod load-sample ((row seq-row))
-  (setf (sample-instance row) (sdl-mixer:load-sample (sample row))))
-
+  (setf (sample-instance row) (sdl-mixer:load-sample (nth (sample row) *samples*))))
 
 (defmethod display ((row seq-row) &key (surface sdl:*default-display*))
   (let ((pos-y (+ (* (pos row) *padding*) *top-padding*))
-        (i 0)
-        )
+        (i 0))
     (dolist (note (code row))
-      (if note
-          (if (equal *tick* i)
-              (sdl-gfx:draw-filled-circle-*
-               (+ (* i *padding*) 20)
-               pos-y
-               (round (* *padding* 0.4))
-               :surface surface
-               :color sdl:*red*
-               )
-              (sdl-gfx:draw-filled-circle-*
-               (+ (* i *padding*) 20)
-               pos-y
-               (round (* *padding* 0.4))
-               :surface surface
-               :color (color row)
-               ))
-          (if (equal *tick* i)
-              (sdl-gfx:draw-circle-*
-               (+ (* i *padding*) 20)
-               pos-y
-               (round (* *padding* 0.4))
-               :surface surface
-               :color sdl:*red*
-               )
-              (sdl-gfx:draw-circle-*
-               (+ (* i *padding*) 20)
-               pos-y
-               (round (* *padding* 0.4))
-               :surface surface
-               :color (color row)
-               )))
-      (incf i))))
+      (let ((pos-x (+ (* i *padding*) 20))
+            (radius (round (* *padding* 0.4))))
+        (if note
+            (if (equal *tick* i)
+                (sdl-gfx:draw-filled-circle-* pos-x pos-y radius
+                                              :surface surface :color sdl:*red*)
+                (sdl-gfx:draw-filled-circle-* pos-x pos-y radius
+                                              :surface surface :color (color row)))
+            (if (equal *tick* i)
+                (sdl-gfx:draw-circle-* pos-x pos-y radius
+                                       :surface surface :color sdl:*red*)
+                (sdl-gfx:draw-circle-* pos-x pos-y radius
+                                       :surface surface :color (color row))))
+        (incf i)))
+    (sdl:draw-string-solid-* (pathname-name (nth (sample row) *samples*))
+                             (+ (* i *padding*) 20)
+                             pos-y
+                             :color (color row)
+                             :surface surface
+                             )
+    ))
 
+(defgeneric play (seq-row))
 (defmethod play ((row seq-row))
   (if (nth *tick* (code row))
       (sdl-mixer:play-sample (sample-instance row))))
+
+(defgeneric change-sample (seq-row direction))
+(defmethod change-sample ((row seq-row) direction)
+  (setf (sample row) (mod (+ (sample row) direction) (length *samples*)))
+  (sdl:Free (sample-instance row))
+  (load-sample row))
 
 (defun clean-up ()
   (when *samples*
     (when (sdl-mixer:sample-playing-p nil)
       (sdl-mixer:pause-sample t)
       (sdl-mixer:Halt-sample :channel t)))
-  ;;   (dolist (sample *samples*)
-  ;;     (sdl:Free sample))
-  ;;   (setf *samples* nil))
+  (dolist (row *rows*)
+    (sdl:Free (sample-instance row)))
   (when *mixer-opened*
     (sdl-mixer:Close-Audio t)
     (setf *mixer-opened* nil))
@@ -116,10 +110,12 @@
     (if (< rownum (length *rows*))
         (let ((row (nth rownum *rows*)))
           (if (< index (w row))
-              (if (nth index (code row))
-                  (setf (nth index (code row)) nil)
-                  (setf (nth index (code row)) t)))
-          (print (code row))))))
+              (progn
+                (if (nth index (code row))
+                    (setf (nth index (code row)) nil)
+                    (setf (nth index (code row)) t))
+                (print (code row)))
+              (change-sample row 1))))))
 
 (defun sample-finished-action ()
   (sdl-mixer:register-sample-finished
@@ -132,30 +128,17 @@
         (status "")
         (timeout 1)      
         (100-frames-p (every-n-frames 100)))
-    (setf *rows* (list (make-instance 'seq-row
-                                      :w 16
-                                      :pos 0
-                                      :color sdl:*cyan*
-                                      :sample (first *samples*))
-                       (make-instance 'seq-row
-                                      :w 16
-                                      :pos 1
-                                      :sample (nth 2 *samples*))
-                       (make-instance 'seq-row
-                                      :w 16
-                                      :pos 2
-                                      :color sdl:*green*
-                                      :sample (nth 4 *samples*))
-                       (make-instance 'seq-row
-                                      :w 16
-                                      :pos 3
-                                      :color sdl:*white*
-                                      :sample (nth 5 *samples*))
-                       ))
-    (print *rows*)
+    (dotimes (i 8)
+      (push (make-instance 'seq-row
+                           :w 16
+                           :pos i
+                           :color (nth (mod i 2) (list sdl:*blue* sdl:*cyan*))
+                           :sample i)
+                           *rows*))
+    (setf *rows* (nreverse *rows*))
     (sdl:with-init ()
       (sdl:window *width* *height*
-                  :title-caption "cltx")
+                  :title-caption "clss")
       (setf (sdl:frame-rate) 60)
       (sdl-gfx:initialise-default-font)
       
@@ -165,7 +148,9 @@
       (sdl:enable-key-repeat 500 50)
       (sdl-mixer:init-mixer :mp3)
       (setf *mixer-opened*
-            (sdl-mixer:OPEN-AUDIO :chunksize 1024 :enable-callbacks nil))
+            (sdl-mixer:OPEN-AUDIO :frequency 48000
+                                  :chunksize 1024
+                                  :enable-callbacks nil))
       (when *mixer-opened*
         (setf status "Opened Audio Mixer!")
         (sample-finished-action)
@@ -198,7 +183,7 @@
                  (display row))
 
                (draw-fps status
-                         10 150 sdl:*default-font* sdl:*default-display*
+                         10 (- *height* 20) sdl:*default-font* sdl:*default-display*
                          (funcall 100-frames-p))
                
                (sdl:update-display))))))
